b59ce3306414f06ef91070e4f667a24dcb100128
Chaitanya Pathipati <cp13@iitbbs.ac.in>
2020-12-12 19:15:56 +0000

    added chat component

--- ReactSignalR/reactsignalr/src/features/chat/Chat.tsx
+++ ReactSignalR/reactsignalr/src/features/chat/Chat.tsx
@@ -1,9 +1,21 @@
-﻿import React from "react";
-
+﻿import React, { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux'
+import { addMessage, selectMessages } from "./chatSlice";
 export function Chat(){
+    const dispatch = useDispatch();
+    const messages = useSelector(selectMessages);
+    let [currentMessage, setMessage] = useState('');
     return (
         <div>
-
+            <div>
+                {messages.map(message => (
+                    <p>{message}</p>
+                ))}
+            </div>
+            <div>
+                <input value={currentMessage} onChange={e => setMessage(e.target.value)} />
+                <button onClick={e => dispatch(addMessage(currentMessage))}>send</button>
+            </div>
         </div>
         );
 }
--- ReactSignalR/reactsignalr/src/features/chat/chatSlice.ts
+++ ReactSignalR/reactsignalr/src/features/chat/chatSlice.ts
@@ -1,9 +1,28 @@
 ﻿import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { stat } from 'fs';
+import { RootState } from '../../app/store'
 
 interface ChatState {
-    count: number;
+    messages: string[];
 }
 
 const initialState: ChatState = {
-    count: 0,
+    messages: [],
 }
+
+let chatSlice = createSlice({
+    name: 'chat',
+    initialState,
+    reducers:{
+        addMessage: (state: ChatState, action:PayloadAction<string>) => {
+            state.messages.push(action.payload)
+            return state;
+        } 
+    }
+})
+
+export const { addMessage } = chatSlice.actions;
+
+export const selectMessages = (state: RootState) => state.chat.messages;
+
+export default chatSlice.reducer;
